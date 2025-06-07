ff609ae6dae2b86d2f2f42839aa4eb31775787d3
Sávio <saviovinysb@gmail.com>
2025-06-06 22:33:26 -0300

    Aula 06/06/2025

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Postagens.Models
 {
-    internal class Post
+    public class PostModel
     {
+        public int id;
+        public int userId;
+        public string Body;
+        public string Title;
     }
 }
--- ViewModels/PostViewModel.cs
+++ ViewModels/PostViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Postagens.ViewModels
 {
-    internal class PostViewModel
+    public partial class PostViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string titulo;
+
+        [ObservableProperty]
+        private string corpo;
+
+
+
     }
 }
